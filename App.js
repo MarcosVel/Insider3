db17cce2e625a8d20dc70f6b1cd0fd6a80378499
MarcosVel <marcos.veloso@sempreceub.com>
2021-06-02 19:45:54 -0300

    add statusBar in Home

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { StyleSheet, StatusBar } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './src/routes';
@@ -8,7 +8,6 @@ import Routes from './src/routes';
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle='dark-content' backgroundColor='transparent' />
       <Routes />
     </NavigationContainer>
   );
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function MyLinks() {
-  return(
-    <View>
+  return (
+    <LinearGradient
+      colors={ [ '#908', '#216362' ] }
+      style={ { flex: 1, justifyContent: 'center' } }
+    >
       <Text>MyLinks</Text>
-    </View>
+    </LinearGradient>
   )
 }
