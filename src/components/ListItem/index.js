084c93442fa9691a7d8429590b21888026f6863a
MarcosVel <marcos.veloso@sempreceub.com>
2021-06-07 23:43:23 -0300

    add flatlist

---
+++ src/components/ListItem/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {} from './styles';
+
+export default function ListItem() {
+  return (
+    <View>
+      <Text>https://seulink.com</Text>
+    </View>
+  )
+}
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
 import Menu from '../../components/Menu';
+import ListItem from '../../components/ListItem';
 import StatusBarPage from '../../components/StatusBarPage';
-import { Container, Title, ListLinks } from './styles';
+import { Container, ListLinks, Title } from './styles';
 
 export default function MyLinks() {
   return (
@@ -19,7 +19,11 @@ export default function MyLinks() {
       <ListLinks
         data={ [ { id: 1, link: 'test.com' } ] }
         keyExtractor={ (item) => String(item.id) }
-        renderItem={ ({ item }) => <ListItem /> }
+        renderItem={ ({ item }) =>
+          <ListItem data={ item } />
+        }
+        contentContainerStyle={ { paddingBottom: 20 } }
+        showsVerticalScrollIndicator={ false }
       />
 
     </Container>
