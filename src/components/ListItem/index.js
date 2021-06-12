5353fe0cddde55a20000613a14cf565faf2da74d
MarcosVel <marcos.veloso@sempreceub.com>
2021-06-12 18:55:08 -0300

    styling MyLinks

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import {} from './styles';
+import { ContainerButton, Item } from './styles';
+import { Feather } from '@expo/vector-icons';
 
 export default function ListItem() {
   return (
     <View>
-      <Text>https://seulink.com</Text>
+      <ContainerButton activeOpacity={ 0.8 } onPress={ () => { } }>
+        <Feather
+          name='link'
+          color='#fff'
+          size={ 24 }
+        />
+        <Item numberOfLines={ 1 }>https://youtube.com</Item>
+      </ContainerButton>
     </View>
   )
 }
--- src/components/ListItem/styles.js
+++ src/components/ListItem/styles.js
@@ -1,1 +1,17 @@
 import styled from 'styled-components/native';
+
+export const ContainerButton = styled.TouchableOpacity`
+  flex-direction: row;
+  background-color: rgba(255, 255, 255, .21);
+  margin: 7px 20px;
+  padding: 12px;
+  border-radius: 7px;
+  align-items: center;
+`;
+
+export const Item = styled.Text`
+  color: #fff;
+  padding-left: 10px;
+  padding-right: 20px;
+  font-size: 18px;
+`;
