772493db94429242f5a72ebcc032066a3b8eb06a
MarcosVel <marcos.veloso@sempreceub.com>
2021-06-16 20:42:33 -0300

    add trash btn

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { ContainerButton, Item } from './styles';
+import { View } from 'react-native';
+import { ContainerButton, Item, ActionContainer } from './styles';
 import { Feather } from '@expo/vector-icons';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 export default function ListItem({ data, selectedItem }) {
-  return (
-    <View>
-      <ContainerButton activeOpacity={ 0.8 } onPress={ () => selectedItem(data) }>
+
+  function RightActions() {
+    return (
+      <ActionContainer>
         <Feather
-          name='link'
-          color='#fff'
+          name="trash"
+          color="#fff"
           size={ 24 }
         />
-        <Item numberOfLines={ 1 }>{ data.long_url }</Item>
-      </ContainerButton>
+      </ActionContainer>
+    )
+  }
+
+  return (
+    <View>
+      <Swipeable renderRightActions={ RightActions } >
+        <ContainerButton activeOpacity={ 0.8 } onPress={ () => selectedItem(data) }>
+          <Feather
+            name='link'
+            color='#fff'
+            size={ 24 }
+          />
+          <Item numberOfLines={ 1 }>{ data.long_url }</Item>
+        </ContainerButton>
+      </Swipeable>
     </View>
   )
 }
